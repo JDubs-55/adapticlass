aafe652d3f317e9817248a396168d4853a2a2676
admin <jack.weller@gmail.com>
2024-03-06 17:01:39 -0500

    Updated dir structure/imports

--- AdaptiClass/frontend/src/components/Upcoming.js
+++ AdaptiClass/frontend/src/components/Upcoming.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import { FaCheckCircle, FaRegCircle } from '../react-icons/fa'; 
+import { FaCheckCircle, FaRegCircle } from 'react-icons/fa';
+
 
 const UpcomingContainer = styled.div`
   width: 634px; 
