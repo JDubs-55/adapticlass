a728f7cf3f2a8105aca964f9de0115f7b729a84e
admin <jack.weller@gmail.com>
2024-03-19 02:02:00 -0400

    Change name temporary

---
+++ AdaptiClass/frontend/src/pages/ExercisesPage.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Questions from '../components/Questions';
+import ProgressBar from '../components/ProgressBar';
+import ChatBox from '../components/Chatbox';
+import assignments from '../assignments.json'; 
+
+const MainLayout = styled.div`
+  display: flex;
+  align-items: flex-start;
+  margin: 20px;
+  gap: 20px;
+`;
+
+const Courses = () => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const totalQuestions = assignments.length;
+  const [quizCompleted, setQuizCompleted] = useState(false);
+
+
+  return (
+    <MainLayout>
+      <div>
+      <Questions 
+        updateCurrentIndex={setCurrentQuestionIndex} 
+        totalQuestions={totalQuestions}
+        setQuizCompleted={setQuizCompleted} 
+      />
+
+      <ProgressBar current={currentQuestionIndex + 1} total={totalQuestions} quizCompleted={quizCompleted} />
+      </div>
+      <ChatBox/>
+    </MainLayout>
+  );
+};
+
+export default Courses;
